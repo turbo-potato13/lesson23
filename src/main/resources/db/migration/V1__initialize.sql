7f304a16c3eb00e67d12398b7de0e519759d350d
Alexandr Kortunov <truba191@yandex.ru>
2020-08-24 15:04:32 +0300

    c

--- src/main/resources/db/migration/V1__initialize.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (id bigserial PRIMARY KEY, name VARCHAR(255), age INTEGER);
-INSERT INTO users (name, age) VALUES
-('Arnold S.', 45),
-('Jean R.', 54);
-
-CREATE TABLE products (id bigserial PRIMARY KEY, title VARCHAR(255), price INTEGER);
-INSERT INTO products (title, price) VALUES
-('vase', 10),
-('sphere', 3);
